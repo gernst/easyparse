fa21d5970031b97c757994d84d7ecf01335c1e34
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2015-06-20 12:34:19 +0200

    don't combinate -> easier debugging.

--- src/arse/Mixfix.scala
+++ src/arse/Mixfix.scala
@@ -11,52 +11,60 @@ trait Mixfix extends Combinators {
   def Min = 0
   def Max = Int.MaxValue
 
-  def inner_op: Parser[T, Op]
-  def inner_arg: Parser[T, Expr]
+  val inner_expr: Parser[T, Expr]
 
-  def prefix_op(lower: Int): Parser[T, (Op, Int)]
-  def postfix_op(lower: Int, upper: Int): Parser[T, (Op, Int)]
-  def infix_op(lower: Int, upper: Int): Parser[T, (Op, Int, Assoc)]
+  def prefix_op: Parser[T, (Op, Int)]
+  def postfix_op: Parser[T, (Op, Int)]
+  def infix_op: Parser[T, (Op, (Assoc, Int))]
 
   def unary(op: Op, arg: Expr): Expr
   def binary(op: Op, arg1: Expr, arg2: Expr): Expr
 
-  def prefix_app(lower: Int) = {
-    prefix_op(lower) >> {
-      case (op, prec) =>
-        mixfix_app(prec) ^^ { unary(op, _) }
+  def prefix_app(lower: Int, in0: Input) = {
+    val ((op, prec), in1) = prefix_op(in0)
+    if(prec < lower) fail
+    val (right, in2) = mixfix_app(prec, in1)
+    (unary(op, right), in2)
+  }
+
+  def postfix_app(lower: Int, upper: Int, left: Expr, in0: Input) = {
+    val ((op, prec), in1) = postfix_op(in0)
+    if(prec < lower || upper < prec) fail
+    postinfix_app(lower, prec, unary(op, left), in1)
+  }
+
+  private def nprec(assoc: Assoc, prec: Int) = if (assoc == Left) prec else prec - 1
+  private def rprec(assoc: Assoc, prec: Int) = if (assoc == Right) prec else prec + 1
+
+  def infix_app(lower: Int, upper: Int, left: Expr, in0: Input) = {
+    val ((op, (assoc, prec)), in1) = infix_op(in0)
+    if(prec < lower || upper < prec) fail
+    val (right, in2) = mixfix_app(rprec(assoc, prec), in1)
+    postinfix_app(lower, nprec(assoc, prec), binary(op, left, right), in2)
+  }
+
+  def postinfix_app(lower: Int, upper: Int, left: Expr, in: Input): (Expr, Input) = {
+    {
+      infix_app(lower, upper, left, in)
+    } or {
+      postfix_app(lower, upper, left, in)
+    } or {
+      (left, in)
     }
   }
 
-  def postfix_app(lower: Int, upper: Int, left: Expr): Parser[T, Expr] = {
-    postfix_op(lower, upper) >> {
-      case (op, prec) =>
-        postinfix_app(lower, prec, unary(op, left))
+  def mixfix_arg(lower: Int, in: Input) = {
+    {
+      prefix_app(lower, in)
+    } or {
+      inner_expr(in)
     }
   }
 
-  def nprec(assoc: Assoc, prec: Int) = if (assoc == Left) prec else prec - 1
-  def rprec(assoc: Assoc, prec: Int) = if (assoc == Right) prec else prec + 1
-
-  def infix_app(lower: Int, upper: Int, left: Expr): Parser[T, Expr] = {
-    infix_op(lower, upper) >> {
-      case (op, prec, assoc) =>
-        mixfix_app(rprec(assoc, prec)) >> {
-          right =>
-            postinfix_app(lower, nprec(assoc, prec), binary(op, left, right))
-        }
-    }
+  def mixfix_app(lower: Int, in0: Input): (Expr, Input) = {
+    val (left, in1) = mixfix_arg(lower, in0)
+    postinfix_app(lower, Max, left, in1)
   }
 
-  def postinfix_app(lower: Int, upper: Int, left: Expr) = {
-    infix_app(lower, upper, left) | postfix_app(lower, upper, left) | ret(left)
-  }
-
-  def mixfix_arg(lower: Int) = {
-    prefix_app(lower) | inner_arg
-  }
-
-  def mixfix_app(lower: Int): Parser[T, Expr] = {
-    mixfix_arg(lower) >> { postinfix_app(lower, Max, _) }
-  }
+  val mixfix_expr: Parser[T, Expr] = lift { mixfix_app(Min, _) }
 }
