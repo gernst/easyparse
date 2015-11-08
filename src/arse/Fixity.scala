00fc045ca90e41231325f5f8bfa61a51a253a187
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2015-11-09 00:02:16 +0100

    pretty printing.

--- src/arse/Fixity.scala
+++ src/arse/Fixity.scala
@@ -5,12 +5,32 @@
 package arse
 
 sealed trait Assoc
-case object Non extends Assoc
-case object Left extends Assoc
-case object Right extends Assoc
+case object Non extends Assoc {
+  override def toString = "non"
+}
+
+case object Left extends Assoc {
+  override def toString = "left"
+}
+
+case object Right extends Assoc {
+  override def toString = "right"
+}
 
 trait Fixity
-case object Nilfix extends Fixity
-case class Prefix(prec: Int) extends Fixity
-case class Postfix(prec: Int) extends Fixity
-case class Infix(assoc: Assoc, prec: Int) extends Fixity
+
+case object Nilfix extends Fixity {
+  override def toString = "nilfix"
+}
+
+case class Prefix(prec: Int) extends Fixity {
+  override def toString = "prefix " + prec
+}
+
+case class Postfix(prec: Int) extends Fixity {
+  override def toString = "postfix " + prec
+}
+
+case class Infix(assoc: Assoc, prec: Int) extends Fixity {
+  override def toString = "infix " + assoc + " " + prec
+}
