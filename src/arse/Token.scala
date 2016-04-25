3c7331143be6891fa56edd3cfa90efbc131dc073
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2016-04-25 08:24:21 +0200

    update year in (c).

--- src/arse/Token.scala
+++ src/arse/Token.scala
@@ -1,10 +1,16 @@
 // ARSE Parser libary
-// (c) 2015 Gidon Ernst <gidonernst@gmail.com>
+// (c) 2016 Gidon Ernst <gidonernst@gmail.com>
 // This code is licensed under MIT license (see LICENSE for details)
 
 package arse
 
-case class Location(source: String, range: Range)
+case class Location(source: String, range: Range) {
+  def this(source: String, start: Int, end: Int) = this(source, start until end)
+  def ++(that: Location) = {
+    assert(this.source == that.source)
+    Location(source, this.range.start until that.range.end)
+  }
+}
 
 case class Token(text: String, location: Location) {
   override def toString = text
